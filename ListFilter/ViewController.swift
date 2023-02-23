3f981cc4bbaa368146deee371f7ecd300b0e4a6a
kaueludovico <kaueludovico@outlook.com>
2023-02-23 00:19:27 -0300

    Initial Commit

---
+++ ListFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListFilter
+//
+//  Created by Kaue Ludovico on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
